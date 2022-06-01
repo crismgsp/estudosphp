c6cf875ca838e09b0bd1295716190b58cf89ace0
crismgsp <crismgsp@gmail.com>
2022-06-01 14:01:12 -0300

    adicionando 2 arquivos sobre filtros

---
+++ filtros.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'MeuFiltro.php';
+
+$arquivosCursos = fopen('lista-cursos.txt', 'r');
+
+stream_filter_register('alura.partes', MeuFiltro::class);
+
+stream_filter_append($arquivosCursos, 'alura.partes');
+
+echo fread($arquivosCursos, filesize('lista-cursos.txt'));
